1aa5a50b7618020e5b126973d4eb8c47dd33b748
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-31 22:05:26 +0530

    Mongo connection successful

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+require('dotenv').config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoUri =process.env.MONGO_URI;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("successfully connected!");
+});
+mongoose.connection.on('error',(err)=>{
+    console.error(err,'Error connecting')
+})
 
 app.get("/", (req, res) => {
   res.json({

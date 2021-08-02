efbff63be45bfde323f33f7154bf81343917ef2f
Adithya Kamath <adithyakamath96@gmail.com>
2021-08-02 14:53:10 +0530

    error handling added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,9 +5,14 @@ const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
-  const user = new User({ email, password });
-  await user.save();
-  res.send("Post request received and saved");
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    res.status(201).send("Post request received and saved");
+  } catch (error) {
+   return  res.status(422).send({ message: "Something went wrong", error });
+  }
 });
 
 module.exports = router;

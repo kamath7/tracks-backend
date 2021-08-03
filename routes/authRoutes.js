9efcd34798fd0d526225d7d0c853c9f1dfbf68c0
Adithya Kamath <adithyakamath96@gmail.com>
2021-08-04 00:41:45 +0530

    Setting up a route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const User = mongoose.model("User");
 const router = express.Router();
 
@@ -10,11 +10,29 @@ router.post("/signup", async (req, res) => {
   try {
     const user = new User({ email, password });
     await user.save();
-    const token = jwt.sign({userId: user._id}, process.env.KEY) //creating a token
-    
-    res.status(201).send({token});
+    const token = jwt.sign({ userId: user._id }, process.env.KEY); //creating a token
+
+    res.status(201).send({ token });
   } catch (error) {
-   return  res.status(422).send({ message: "Something went wrong", error });
+    return res.status(422).send({ message: "Something went wrong", error });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ message: "Must provide email or password" });
+  }
+  const user = await User.findOne({email})
+
+  if(!user){
+    return res.status(404).send({message: 'No one found out with that email!'})
+  }
+  try {
+    await user.comparePassword(password);
+
+  } catch (error) {
+      return res.status(422).send({message: 'Incorrect credentials entered!'})
   }
 });
 

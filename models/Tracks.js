6ab3b657352a7c7ff0ef7f5852f6c2b5c733a1d4
Adithya Kamath <adithyakamath96@gmail.com>
2021-08-04 01:02:55 +0530

    Track schema created

---
+++ models/Tracks.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PointSchema = new mongoose.Schema({
+  timestamp: Number,
+  coords: {
+    latitude: Number,
+    longitude: Number,
+    altitude: Number,
+    accuracy: Number,
+    heading: Number,
+    speed: Number,
+  },
+});
+const TrackSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    default: "",
+  },
+  locations: [PointSchema],
+});
+
+mongoose.model("Track", TrackSchema);

382326ba1172f2ec5524e3d3140c90009b68f9fc
Adithya Kamath <adithyakamath96@gmail.com>
2021-08-04 01:08:22 +0530

    Router file complete

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const requireAuth = require("../middleware/requireAuth")
+const requireAuth = require("../middleware/requireAuth");
 
-const Track = mongoose.model('Track')
+const Track = mongoose.model("Track");
+const router = express.Router();
+
+router.use(requireAuth);
+
+router.get("/tracks", async (req, res) => {
+  const tracks = await Track.find({ userId: req.user._id });
+
+  res.send(tracks);
+});
+
+module.exports = router;
